786d48fd7e6f02b6f4d82f4545cd10544574338e
Lala <ace.rexnoir@gmail.com>
2024-07-04 22:59:05 +0530

    feat : separate between all and own for granular approach

--- app/enums/Permissions.php
+++ app/enums/Permissions.php
@@ -4,22 +4,39 @@ namespace App\Enums;
 
 enum Permissions: string
 {
-    case CREATE_POST = 'create post';
-    case EDIT_POST = 'edit post';
-    case DELETE_POST = 'delete_post';
-    case VIEW_POST = 'view_post';
+        //ACCESSES
     case ACCESS_ADMIN_PANEL = 'access admin panel';
-    case UPDATE_POST = 'update post';
+
+        //plurals
+    case CREATE_POSTS = 'create posts';
+    case EDIT_ALL_POSTS = 'edit post';
+    case DELETE_ALL_POSTS = 'delete_posts';
+    case VIEW_ALL_POST = 'view_posts';
+    case UPDATE_ALL_POSTS = 'update posts';
+
+        //singulars
+    case UPDATE_OWN_POSTS = 'update own posts';
+    case EDIT_OWN_POSTS = 'edit own posts';
+    case VIEW_OWN_POSTS = 'view own posts';
+    case DELETE_OWN_POSTS = 'delete own posts';
 
     public function label(): string
     {
         return match ($this) {
+
+            //plurals
             static::ACCESS_ADMIN_PANEL => 'Access to admin panel',
-            static::CREATE_POST => 'Create post',
-            static::DELETE_POST => 'Delete post',
-            static::EDIT_POST => 'Edit post',
-            static::VIEW_POST => 'View post',
-            static::UPDATE_POST => 'Update post'
+            static::CREATE_POSTS => 'Create posts',
+            static::DELETE_ALL_POSTS => 'Delete all posts',
+            static::EDIT_ALL_POSTS => 'Edit all posts',
+            static::VIEW_ALL_POST => 'View all posts',
+            static::UPDATE_ALL_POSTS => 'Update all posts',
+
+            //singulars
+            static::UPDATE_OWN_POSTS => 'Update own posts',
+            static::EDIT_OWN_POSTS => 'Edit own posts',
+            static::DELETE_OWN_POSTS => 'Delete own posts',
+            static::VIEW_OWN_POSTS => 'View own posts',
         };
     }
 }
