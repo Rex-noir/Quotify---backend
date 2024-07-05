815ac3fb550e86eb152b3711744ca1b364012656
Lala <ace.rexnoir@gmail.com>
2024-07-05 13:13:09 +0530

    feat : Add tabs filter by roles

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -4,7 +4,10 @@ namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+
 
 class ListUsers extends ListRecords
 {
@@ -14,6 +17,25 @@ class ListUsers extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('all'),
+            'active' => Tab::make('Users')
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereHas('roles', function ($query) {
+                    return $query->where('name', 'user');
+                })),
+            'inactive' => Tab::make('Admins')
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereHas('roles', function ($query) {
+                    $query->where('name', 'admin');
+                })),
+            'inactive' => Tab::make('Super Admins')
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereHas('roles', function ($query) {
+                    $query->where('name', 'super admin');
+                })),
         ];
     }
 }
