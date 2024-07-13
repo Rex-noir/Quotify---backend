6041cec329865496329f5406be9c9f98e8207fef
Lala <ace.rexnoir@gmail.com>
2024-07-13 22:47:21 +0530

    feat : add deeper nesting

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -14,13 +14,25 @@ class CommentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
         $posts = Post::all();
 
         foreach ($posts as $post) {
-            $comment = Comment::factory()->for($post)->create();
 
-            Comment::factory()->for($post)->count(3)->create(['parent_id' => $comment->id]);
+            $comment = Comment::factory()->for($post)->create();
+            $another = Comment::factory(3)->for($post)->create();
+
+            $level1Replies = Comment::factory()->for($post)->count(2)->create(['parent_id' => $comment->id]);
+
+            foreach ($level1Replies as $level1Reply) {
+                $level2Replies = Comment::factory()->for($post)->count(2)->create(['parent_id' => $level1Reply->id]);
+
+                foreach ($level2Replies as $level2Reply) {
+                    $replies =   Comment::factory()->for($post)->count(10)->create(['parent_id' => $level2Reply->id]);
+                    foreach ($replies as $level3Reply) {
+                        $replies =   Comment::factory()->for($post)->count(2)->create(['parent_id' => $level3Reply->id]);
+                    }
+                }
+            }
         }
     }
 }
