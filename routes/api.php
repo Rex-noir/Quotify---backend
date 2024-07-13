7484a8ac7bb176c6ae853dd03a2a95f3b0876f76
Lala <ace.rexnoir@gmail.com>
2024-07-13 22:47:51 +0530

    feat : add routes for, getting posts, users,comments and replies

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,7 +11,16 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::apiResource('/users', UserController::class);
+
+
+//Posts
 Route::apiResource('/posts', PostController::class);
+Route::get('/posts/{post}/comments', [PostController::class, 'comments']);
+
+//Comments
+Route::apiResource('/comments', CommentController::class);
+Route::get('/comments/replies/{comment}', [CommentController::class, 'replies']);
 
 Route::prefix('/auth')->group(function () {
     Route::post('/verify', [AuthController::class, 'verify']);
