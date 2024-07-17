2c52f328781bd311ec51ebba9a72f86ee466762f
Lala <ace.rexnoir@gmail.com>
2024-07-17 17:22:13 +0530

    feat : event for when a new comment is added

---
+++ app/Events/CommentAdded.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CommentAdded implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $comment;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(Comment $comment)
+    {
+        //
+        $this->comment = $comment->load('user');
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('posts'),
+        ];
+    }
+}
