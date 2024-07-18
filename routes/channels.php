2bfebc2e9a913ec978ded2d6a497b455248ed9e2
Lala <ace.rexnoir@gmail.com>
2024-07-19 01:32:25 +0530

    feat : define channel for 'comment'

--- routes/channels.php
+++ routes/channels.php
@@ -2,11 +2,15 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Log;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
 Broadcast::channel('posts', function () {
+    return true;
+});
+Broadcast::channel('comments', function () {
     return true;
 });
