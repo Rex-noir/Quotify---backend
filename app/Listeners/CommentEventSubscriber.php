1e463cff5f9561ab19a238faec39fa4e7fdf3b65
Lala <ace.rexnoir@gmail.com>
2024-07-25 10:04:20 +0530

    refactor : remove handleCommentLikeDislikeClicked

--- app/Listeners/CommentEventSubscriber.php
+++ app/Listeners/CommentEventSubscriber.php
@@ -3,7 +3,6 @@
 namespace App\Listeners;
 
 use App\Events\CommentAdded;
-use App\Events\CommentLikeDislikeClicked;
 use App\Events\CommentUpdated;
 use App\Models\Comment;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -18,33 +17,8 @@ class CommentEventSubscriber
         $comment = $event->comment;
         if ($comment && isset($comment->parent_id)) {
             $parentComment = Comment::findOrFail($comment['parent_id']);
-            $updates = ['replies_count' => $parentComment->replies()->count(), 'id' => $comment['parent_id'], 'post_id' => $comment['post_id']];
-            broadcast(new CommentUpdated($updates))->toOthers();
-        }
-    }
-
-    public function handleCommentLikeDislikeClicked(CommentLikeDislikeClicked $event)
-    {
-        try {
-            $comment = Comment::findOrFail($event->commentId);
-            $changes = $event->likeOrDislike;
-
-            $updates = [
-                'id' => $event->commentId,
-                'post_id' => $comment->post_id,
-            ];
-
-            if ($changes['is_like']) {
-                $updates = array_merge($updates, ['likes_count' => $comment->likes()->count()]);
-            }
-
-            if ($changes['is_dislike']) {
-                $updates = array_merge($updates, ['dislikes_count' => $comment->dislikes()->count()]);
-            }
-
-            event(new CommentUpdated($updates));
-        } catch (\Throwable $th) {
-            Log::error('Error handling LikeDislikeClicked event' . $th->getMessage());
+            $public = ['replies_count' => $parentComment->replies()->count(), 'id' => $comment['parent_id'], 'post_id' => $comment['post_id']];
+            broadcast(new CommentUpdated($public))->toOthers();
         }
     }
 
@@ -53,7 +27,6 @@ class CommentEventSubscriber
 
         return [
             CommentAdded::class => 'handleCommentAdded',
-            CommentLikeDislikeClicked::class => 'handleCommentLikeDislikeClicked'
         ];
     }
 }
