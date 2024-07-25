89fc1f33a349cca1b46dd268ef809ae4dd266b22
Lala <ace.rexnoir@gmail.com>
2024-07-25 17:40:31 +0530

    refactor : remove handle for like dislike clicked

--- app/Listeners/PostEventsSubscriber.php
+++ app/Listeners/PostEventsSubscriber.php
@@ -3,7 +3,6 @@
 namespace App\Listeners;
 
 use App\Events\CommentAdded;
-use App\Events\LikeDislikeClicked;
 use App\Events\PostUpdated;
 use App\Models\Post;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -28,35 +27,10 @@ class PostEventsSubscriber
         }
     }
 
-    public function handleLikeDislikeClicked(LikeDislikeClicked $event)
-    {
-        try {
-            $post = Post::findOrFail($event->postId);
-            $changes = $event->likeOrDislike;
-
-            $updates = [
-                'id' => $event->postId,
-            ];
-
-            if ($changes['is_like']) {
-                $updates = array_merge($updates, ['likes_count' => $post->likes()->count()]);
-            }
-
-            if ($changes['is_dislike']) {
-                $updates = array_merge($updates, ['dislikes_count' => $post->dislikes()->count()]);
-            }
-
-            event(new PostUpdated($updates));
-        } catch (\Throwable $th) {
-            Log::error('Error handling LikeDislikeClicked event' . $th->getMessage());
-        }
-    }
-
     public function subscribe(Dispatcher $events): array
     {
         return [
             CommentAdded::class => 'handleCommentAdded',
-            LikeDislikeClicked::class => 'handleLikeDislikeClicked'
         ];
     }
 }
