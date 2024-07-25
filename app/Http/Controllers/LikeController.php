8f2c92408a48c9d15b8e87370a59134d01dd8daf
Lala <ace.rexnoir@gmail.com>
2024-07-25 17:39:51 +0530

    feat : send private data to user specific events

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,49 +2,70 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\LikeDislikeClicked;
+use App\Events\PostUpdated;
+use App\Events\UserSpecificPostUpdates;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LikeController extends Controller
 {
-    //
     public function likePost(Request $request, $id)
     {
         $post = Post::findOrFail($id);
         $user = Auth::user();
 
+        $public = ['id' => $post->id,];
+
         $existingLike = $post->likes()->where('user_id', $user->id)->first();
         $existingDislike = $post->dislikes()->where('user_id', $user->id)->first();
 
         if ($existingLike) {
-            // If the user already liked the post, remove the like
             $existingLike->delete();
-            $eventData = ['is_like' => false, 'is_dislike' => false];
+            $isLiked = false;
         } else {
-            // If the user has disliked the post, remove the dislike first
             if ($existingDislike) {
                 $existingDislike->delete();
             }
-            // Create a new like
             $post->likes()->create([
                 'user_id' => $user->id,
                 'is_like' => true,
             ]);
-
-            $eventData = (['is_like' => true, 'is_dislike' => false]);
+            $isLiked = true;
         }
 
-        LikeDislikeClicked::dispatch($eventData, $post->id);
+        $isDisliked = false; // After liking, user cannot have disliked the post
 
-        return response()->noContent();
+        // Get the updated counts
+        $likesCount = $post->likes()->count();
+        $dislikesCount = $post->dislikes()->count();
+
+        // Prepare event data
+        $public = array_merge($public, [
+            'likes_count' => $likesCount,
+            'dislikes_count' => $dislikesCount,
+        ]);
+
+        broadcast(new PostUpdated($public));
+
+        if (Auth::check()) {
+            $private = [
+                'id' => $post->id,
+                'is_liked_by_user' => $isLiked,
+                'is_disliked_by_user' => $isDisliked,
+            ];
+            broadcast(new UserSpecificPostUpdates($private));
+        }
+
+        return response(204);
     }
 
     public function dislikePost(Request $request, $id)
     {
-        $post = Post::findOrFail($id);
+        $post = post::findOrFail($id);
         $user = Auth::user();
+
+        $public = ['id' => $post->id,];
 
         $existingLike = $post->likes()->where('user_id', $user->id)->first();
         $existingDislike = $post->dislikes()->where('user_id', $user->id)->first();
@@ -52,7 +73,7 @@ class LikeController extends Controller
         if ($existingDislike) {
             // If the user already disliked the post, remove the dislike
             $existingDislike->delete();
-            $eventData = ['is_like' => false, 'is_dislike' => false];
+            $isDisliked = false;
         } else {
             // If the user has liked the post, remove the like first
             if ($existingLike) {
@@ -63,11 +84,32 @@ class LikeController extends Controller
                 'user_id' => $user->id,
                 'is_like' => false,
             ]);
-            $eventData = ['is_like' => false, 'is_dislike' => true];
+            $isDisliked = true;
         }
 
-        LikeDislikeClicked::dispatch($eventData, $post->id);
+        $isLiked = false; // After disliking, user cannot have liked the post
 
-        return response()->noContent();
+        // Get the updated counts
+        $likesCount = $post->likes()->count();
+        $dislikesCount = $post->dislikes()->count();
+
+        // Prepare event data
+        $public = array_merge($public, [
+            'likes_count' => $likesCount,
+            'dislikes_count' => $dislikesCount,
+        ]);
+
+        broadcast(new PostUpdated($public));
+
+        if (Auth::check()) {
+            $private = [
+                'id' => $post->id,
+                'is_liked_by_user' => $isLiked,
+                'is_disliked_by_user' => $isDisliked,
+            ];
+            broadcast(new UserSpecificPostUpdates($private));
+        }
+
+        return response(204);
     }
 }
